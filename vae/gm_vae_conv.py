bugfixed and numerical stability fixes

vae/gm_vae_conv.py
@@ -159,7 +159,7 @@ class GM_VAE:
 
         with tf.variable_scope("cluster_predictor", reuse=shape_weights):
 
-            for idx, neurons in range(len(self.cluster_predictor_neurons)):
+            for idx, neurons in enumerate(self.cluster_predictor_neurons):
                 with tf.variable_scope("fc{:d}".format(idx + 1)):
                     x = tf.layers.dense(
                         x, neurons, activation=tf.nn.relu,
@@ -173,10 +173,12 @@ class GM_VAE:
         with tf.variable_scope("middle", reuse=share_weights):
 
             # predict z
-            z_t = tf.layers.dense(
+            z_logits_t = tf.layers.dense(
                 input_1_t, self.num_clusters, activation=tf.nn.softmax,
                 kernel_regularizer=utils.get_weight_regularizer(self.weight_decay)
             )
+            z_softmax_t = tf.nn.softmax(z_logits_t)
+            z_logsoftmax_t = tf.nn.log_softmax(z_logits_t)
 
             # predict x
             x_mu_t = tf.layers.dense(
@@ -235,15 +237,18 @@ class GM_VAE:
                 tf.reduce_sum(tf.pow(c_mu_t - x_sample_t[:, tf.newaxis, :], 2) / c_var_t, axis=2) +
                 tf.reduce_sum(c_log_var_t, axis=2) + self.x_size * np.log(2 * np.pi)
             )
-            z_pred_t = tf.nn.softmax(z_pred_log_likelihood_t, axis=1)
+            z_pred_softmax_t = tf.nn.softmax(z_pred_log_likelihood_t, axis=1)
+            z_pred_logsoftmax_t = tf.nn.log_softmax(z_pred_log_likelihood_t, axis=1)
 
             # kl divergences
             w_kl_divergence_t = 0.5 * (tf.square(w_mu_t) + w_var_t - w_log_var_t - 1.0)
+
             x_kl_divergence_t = 0.5 * (
                 c_log_var_t - x_log_var_t[:, tf.newaxis, :] - 1.0 + (x_var_t[:, tf.newaxis, :] / c_var_t) +
-                tf.pow(x_mu_t[: tf.newaxis, :] - c_mu_t, 2) / c_var_t
-            ) * z_pred_t
-            z_kl_divergence_t = - z_pred_t * tf.log(z_t / z_pred_t)
+                tf.pow(x_mu_t[:, tf.newaxis, :] - c_mu_t, 2) / c_var_t
+            ) * z_pred_softmax_t[:, :, tf.newaxis]
+
+            z_kl_divergence_t = - z_pred_softmax_t * (z_logsoftmax_t - z_pred_logsoftmax_t)
 
         return x_sample_t, x_mu_t, x_sd_t, x_var_t, w_kl_divergence_t, x_kl_divergence_t, z_kl_divergence_t
 