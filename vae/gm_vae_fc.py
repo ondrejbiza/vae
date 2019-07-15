fix z kl divergence

vae/gm_vae_fc.py
@@ -228,9 +228,9 @@ class GM_VAE:
             c_sd_t = tf.square(c_var_t)
 
             # predict z from x and clusters
-            z_pred_logits = - (self.num_clusters / 2) * np.log(2 * np.pi) - \
-                            (1 / 2) * tf.reduce_sum(c_log_var_t, axis=2) - \
-                            (1 / 2) * tf.reduce_sum(tf.pow(c_mu_t - x_sample_t[:, tf.newaxis, :], 2) / c_var_t, axis=2)
+            z_pred_logits = - np.log(self.num_clusters) - (self.x_size / 2) * np.log(2 * np.pi) - \
+                (1 / 2) * tf.reduce_sum(c_log_var_t, axis=2) - \
+                (1 / 2) * tf.reduce_sum(tf.pow(c_mu_t - x_sample_t[:, tf.newaxis, :], 2) / c_var_t, axis=2)
             z_pred_softmax = tf.nn.softmax(z_pred_logits, axis=1)
             z_pred_logsoftmax = tf.nn.log_softmax(z_pred_logits, axis=1)
 